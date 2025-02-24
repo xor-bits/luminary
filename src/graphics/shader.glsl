866c4e7a7d3488d2fcc0b0fbf8b1d915950beeee
Eemeli <oneleh@utu.fi>
2025-02-24 20:30:12 +0200

    shadows

--- src/graphics/shader.glsl
+++ src/graphics/shader.glsl
@@ -34,26 +34,34 @@ uint get_voxel(ivec3 world_pos) {
 }
 
 struct HitData {
+    ivec3 voxel;
     vec3 position;
     vec3 normal;
+    float distance;
     uint steps;
     bool hit;
 };
 
-void ray_cast(vec3 ray_origin, vec3 ray_dir, out HitData hit_data) {
-    hit_data.steps = 0;
-    hit_data.hit = false;
+void ray_cast(vec3 ray_origin, vec3 ray_dir, bool skip_first, out HitData hit_data) {
     
     vec3 ray_origin_grid = floor(ray_origin);
     ivec3 world_pos = ivec3(ray_origin_grid);
 
-    vec3 ray_dir_inv = 1.0 / ray_dir;
+    vec3 ray_dist = 1.0 / abs(ray_dir);
     ivec3 ray_sign = ivec3(sign(ray_dir));
     
-    vec3 ray_dist = abs(ray_dir_inv);
-    vec3 next_dist = (ray_sign * (ray_origin_grid - ray_origin.xyz) + (ray_sign * 0.5) + 0.5) * ray_dist;
+    vec3 next_dist = (ray_sign * (ray_origin_grid - ray_origin) + (ray_sign * 0.5) + 0.5) * ray_dist;
 
     bvec3 mask = bvec3(false);
+
+    if (skip_first) {
+        mask = lessThanEqual(next_dist.xyz, min(next_dist.yzx, next_dist.zxy));
+        next_dist += vec3(mask) * ray_dist;
+        world_pos += ivec3(vec3(mask) * ray_sign);
+    }
+
+    hit_data.hit = false;
+    hit_data.steps = 0;
     for (; hit_data.steps < 100; hit_data.steps ++) {
         uint voxel_col = get_voxel(world_pos);
         if (voxel_col != 0) {
@@ -66,16 +74,10 @@ void ray_cast(vec3 ray_origin, vec3 ray_dir, out HitData hit_data) {
         world_pos += ivec3(vec3(mask) * ray_sign);
     }
 
-    hit_data.position = vec3(world_pos);
+    hit_data.voxel = world_pos;
     hit_data.normal = -vec3(ray_sign) * vec3(mask);
-    // hit_data.normal = vec3(1.1) - (-vec3(ray_sign) * vec3(mask));
-    // hit_data.normal = vec3(0.0);
-    // if (mask.x)
-    //     hit_data.normal = vec3(0.5);
-    // if (mask.y)
-    //     hit_data.normal = vec3(1.0);
-    // if (mask.z)
-    //     hit_data.normal = vec3(0.75);
+    hit_data.distance = length(vec3(mask) * (next_dist - ray_dist));
+    hit_data.position = ray_origin + ray_dir * hit_data.distance;
 }
 
 void main() {
@@ -93,14 +95,28 @@ void main() {
     ray_target.xyz /= ray_target.w;
     vec3 ray_dir = normalize(ray_target.xyz - ray_origin.xyz);
 
+    // camera to world cast
     HitData hit_data;
-    ray_cast(ray_origin.xyz, ray_dir, hit_data);
+    ray_cast(ray_origin.xyz, ray_dir, false, hit_data);
 
-    // vec3 col = vec3(hit_data.steps) / vec3(100.0);
-    vec3 col = vec3(0.0);
-    if (hit_data.hit) {
-        col = hit_data.normal;
+    if (!hit_data.hit) {
+        imageStore(image, coord, vec4(0.0, 0.0, 0.0, 1.0));
+        return;
     }
 
-    imageStore(image, coord, vec4(col, 1.0));
+    // imageStore(image, coord, vec4(vec3(hit_data.distance / 50.0), 1.0));
+    // return;
+
+    // shadow cast
+    vec3 sun_dir = vec3(0.5, 1.0, 0.75);
+    HitData light_hit_data;
+    ray_cast(hit_data.position + hit_data.normal * 0.001, sun_dir, true, light_hit_data);
+
+    if (!light_hit_data.hit) {
+        imageStore(image, coord, vec4(vec3(dot(sun_dir, hit_data.normal)), 1.0));
+        return;
+    }
+
+    imageStore(image, coord, vec4(0.1, 0.1, 0.1, 1.0));
+    return;
 }
